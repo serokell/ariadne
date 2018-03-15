d304e8ce127e36eade37e2d8810715ebf84175ea
DanielRrr <chelsea2005joecole@icloud.com>
2018-03-15 15:18:55 +0300

    [AD-1] added Repl for TUI (WIP)

--- src/Ariadne/WalletLayout.hs
+++ src/Ariadne/WalletLayout.hs
@@ -1,14 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Ariadne.WalletLayout  where
 
+import Lens.Micro
+import Lens.Micro.TH
 
 import Control.Monad (void)
-import Data.Monoid ((<>))
-import Text.Wrap (defaultWrapSettings, preserveIndentation)
-
-import Brick (strWrapWith)
-
-import qualified Brick.Main as M
 
 import Brick.Widgets.Core
   ( vBox
@@ -19,38 +17,71 @@ import Brick.Widgets.Core
   , str
   , padTopBottom
   , padTop
+  , (<+>)
+  , hLimit
+  , vLimit
   )
 import Brick.Util (on, bg)
-
-
 import qualified Brick.AttrMap as A
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Center as C
 import qualified Brick.Widgets.Dialog as D
 import qualified Brick.Widgets.Edit as E
+import qualified Brick.Main as M
+import qualified Brick.Focus as F
 import qualified Graphics.Vty as V
 import qualified Brick.Types as T
 
-data Menu = File | View | Help | Settings | WIP 
+data Edit = Edit
+    deriving (Show, Eq, Ord)
 
-drawUI :: D.Dialog Menu -> [T.Widget ()]
-drawUI d = [ vBox [ topUI
-                  , mainUI
-                  , B.hBorder
-                  , auxxUI 
-                  ]
-           ]
+data Menu = File
+          | View
+          | Help
+          | Settings
+          | WIP
+    deriving (Show, Eq, Ord)
+
+data St =
+  St { _menu :: D.Dialog Menu
+     , _middle :: String
+     , _focusRing :: F.FocusRing Edit
+     , _repl :: E.Editor String Edit
+     }
+
+makeLenses ''St
+
+drawUI :: St -> [T.Widget Edit]
+drawUI st = [ vBox [ topUI
+                   , mainUI
+                   , B.hBorder
+                   , auxxUI
+                   ]
+            ]
   where
-    auxxUI = vBox [padTopBottom 5 $ C.hCenter $ str "Auxx Repl", B.hBorder]
+    auxxUI = vBox [ padTopBottom 5 $
+                    C.center $
+                    (str "Auxx Repl" <+> (hLimit 5 $ vLimit 50 e))
+                  , B.hBorder
+                  ]
     mainUI = hBox [ padLeft (T.Pad 20) $ str "side"
                   , B.vBorder
                   , padRight T.Max $ str "main"
                   ]
-    topUI  = vBox [D.renderDialog d $ C.hCenter $ padTopBottom 1 $ str "top"]
+    topUI  = vBox [ D.renderDialog (st^.menu) $
+                    C.hCenter $
+                    padTopBottom 1 $
+                    str "Menu Bar"
+                  ]
+    e = F.withFocusRing (st^.focusRing) (E.renderEditor (str . unlines)) (st^.repl)
 
 
-initialState :: D.Dialog Menu
-initialState = D.dialog Nothing (Just (0, items)) maxBound
+initialState :: St
+initialState =
+  St  (D.dialog Nothing (Just (0, items)) maxBound)
+      (" ")
+      (F.focusRing [Edit])
+      (E.editor Edit (Just 2) " ")
   where
     items = [ ("File", File)
             , ("View", View)
@@ -59,28 +90,30 @@ initialState = D.dialog Nothing (Just (0, items)) maxBound
             , ("WIP", WIP)
             ]
 
-appEvent :: D.Dialog Menu -> T.BrickEvent () e -> T.EventM () (T.Next (D.Dialog Menu))
-appEvent d (T.VtyEvent ev) =
-  case ev of
-    V.EvKey V.KEsc [] -> M.halt d
-    V.EvKey V.KEnter [] -> M.halt d
-    _ -> M.continue =<< D.handleDialogEvent ev d
-appEvent d _ = M.continue d
+appEvent :: St
+         -> T.BrickEvent Edit e
+         -> T.EventM Edit (T.Next St)
+appEvent d (T.VtyEvent ev) = undefined
+
+appCursor :: St -> [T.CursorLocation Edit] -> Maybe (T.CursorLocation Edit)
+appCursor = F.focusRingCursor (^.focusRing)
 
 theMap :: A.AttrMap
 theMap = A.attrMap V.defAttr
     [ (D.dialogAttr, V.white `on` V.black)
     , (D.buttonAttr, V.black `on` V.white)
     , (D.buttonSelectedAttr, bg V.blue)
+    , (E.editAttr, V.white `on` V.black)
+    , (E.editFocusedAttr, V.white `on` V.black)
     ]
 
-app :: M.App (D.Dialog Menu) e ()
+app :: M.App St e Edit
 app =
     M.App { M.appDraw = drawUI
-          , M.appHandleEvent = appEvent
+          , M.appHandleEvent = undefined
           , M.appStartEvent = return
           , M.appAttrMap = const theMap
-          , M.appChooseCursor = M.showFirstCursor
+          , M.appChooseCursor = appCursor
           }
 
 main :: IO ()
