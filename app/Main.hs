f18c639c0bb34e40535b715c859321e5f070d4f2
DanielRrr <chelsea2005joecole@icloud.com>
2018-03-19 20:20:48 +0300

    [AD-1] update in cabal file

--- app/Main.hs
+++ app/Main.hs
@@ -6,4 +6,5 @@ import Ariadne.ConfigForm (runConfigForm)
 main :: IO ()
 main = do
   runConfigForm
+  -- TODO: if config form is non-empty then runBySt initialState else runConfigForm--
   runBySt initialState
