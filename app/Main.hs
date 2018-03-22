e7b4af03c311b3cca72eddfecaf4fb5818e02cbd
Vladislav Zavialov <vlad.z.4096@gmail.com>
2018-03-22 04:53:56 +0300

    Remove legacy code

--- app/Main.hs
+++ app/Main.hs
@@ -11,39 +11,3 @@ main = do
   (uiFace, uiAction) <- createAriadneUI
   (auxxFace, auxxAction) <- createAuxxBackend
   race_ (uiAction auxxFace) (auxxAction uiFace)
-
-{-
-import Prelude
-import Control.Monad (void)
-
-import UI.Wallet (initialAppState, runBySt, menu, focusRing, repl, wallets
-                 , clicked, lastReportedClick, Menu(..), AppState(..)
-                 )
-import UI.Config (runConfigForm)
-import UI.Help (runHelp)
-import Brick.Widgets.Dialog (dialogSelection)
-import Lens.Micro ((^.))
-
-main :: IO ()
-main = do
-  runConfigForm
-  d <- runBySt initialAppState
-  let currentState = AppState  
-                     (d ^. menu) 
-                     (d ^. focusRing) 
-                     (d ^. repl) 
-                     (d ^. wallets)
-                     (d ^. clicked)
-                     (d ^. lastReportedClick)
-  let runLayout = void $ runBySt currentState
-  case (dialogSelection $ currentState ^. menu) of
-    Nothing -> runLayout
-    Just m -> case m of
-      Exit -> return ()
-      Help -> do
-        runHelp
-        runLayout
-      Config -> do
-        runConfigForm
-        runLayout
--}
