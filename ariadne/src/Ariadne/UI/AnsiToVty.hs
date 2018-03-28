a5638e855eaaf4c87a22ce164e230a24b6cefc0b
Dolgakov <dolgakovv@gmail.com>
2018-03-29 01:09:56 +0300

    [AD-1] Add color support

--- ariadne/src/Ariadne/UI/AnsiToVty.hs
+++ ariadne/src/Ariadne/UI/AnsiToVty.hs
@@ -1,10 +1,12 @@
 module Ariadne.UI.AnsiToVty (ansiToVty) where
 
 import Prelude
+
 import Control.Monad.Trans.Writer
 
-import qualified Text.PrettyPrint.ANSI.Leijen as Ppr.A
 import qualified Graphics.Vty as V
+import qualified System.Console.ANSI.Types as AT
+import qualified Text.PrettyPrint.ANSI.Leijen as Ppr.A
 
 ansiToVty :: Ppr.A.SimpleDoc -> V.Image
 ansiToVty = V.vertCat . execWriter . go V.defAttr id
@@ -22,6 +24,27 @@ ansiToVty = V.vertCat . execWriter . go V.defAttr id
       Ppr.A.SLine i x -> do
         tell [img V.emptyImage]
         go attr (V.horizJoin (V.backgroundFill i 1)) x
-      Ppr.A.SSGR _s x ->
-        -- todo: attr
-        go attr img x
+      Ppr.A.SSGR s x ->
+        go (V.withForeColor attr (getColor s)) img x
+
+getColor :: [AT.SGR] -> V.Color
+getColor xs = case colors of
+  []    -> V.white
+  (c:_) -> toColor c
+  where
+    toColor (AT.SetColor _ _ c) = case c of
+      AT.Black   -> V.black
+      AT.Red     -> V.red
+      AT.Green   -> V.green
+      AT.Yellow  -> V.yellow
+      AT.Blue    -> V.blue
+      AT.Magenta -> V.magenta
+      AT.Cyan    -> V.cyan
+      AT.White   -> V.white
+    toColor _ = V.white
+
+    colors = filter isColor xs
+
+    isColor = \case
+      AT.SetColor _ _ _ -> True
+      _ -> False
