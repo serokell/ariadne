602836d115483b35b0c65399f4af8951923cb7f3
Ivan Gromakovskii <gie1994@gmail.com>
2018-03-30 22:12:16 +0300

    [AD-6] Add conversion from UserSecret to [WalletTree]

--- ariadne/src/Ariadne/Glue.hs
+++ ariadne/src/Ariadne/Glue.hs
@@ -1,21 +1,38 @@
 -- | Glue code between the frontend and the backends.
 
-module Ariadne.Glue where
+module Ariadne.Glue
+       (
+         -- * Knit ↔ Vty
+         knitFaceToUI
+       , putKnitEventToUI
+
+         -- * Cardano ↔ Vty
+       , userSecretToTree
+       ) where
+
+import Universum
 
 import Control.Exception (displayException)
-import Data.Foldable
+import Control.Lens (at, non)
+import qualified Data.Foldable
 import Data.Text (pack)
+import Data.Tree (Tree(..))
 import Data.Unique
 import IiExtras
 import Numeric
-import Prelude
+import Prelude ((!!))
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
+import Ariadne.Cardano.Face (UserSecret, WalletUserSecret(..), usWallet)
 import Ariadne.CommandId
 import Ariadne.Knit.Face
 import Ariadne.UI.Vty.Face
 
 import qualified Knit
+
+----------------------------------------------------------------------------
+-- Glue between Knit backend and Vty frontend
+----------------------------------------------------------------------------
 
 knitFaceToUI
   :: forall components.
@@ -79,4 +96,45 @@ putKnitEventToUI
   -> KnitEvent components
   -> IO ()
 putKnitEventToUI UiFace{..} ev =
-  traverse_ putUiEvent (knitEventToUI ev)
+  Data.Foldable.traverse_ putUiEvent (knitEventToUI ev)
+
+----------------------------------------------------------------------------
+-- Glue between Cardano backend and Vty frontend
+----------------------------------------------------------------------------
+
+userSecretToTree :: UserSecret -> [WalletTree]
+userSecretToTree = map toTree . maybeToList . view usWallet
+  where
+    toTree :: WalletUserSecret -> WalletTree
+    toTree WalletUserSecret {..} =
+        Node
+            { rootLabel = WalletTreeItem (Just _wusWalletName) [] False
+            , subForest = map toAccountNode _wusAccounts
+            }
+      where
+        foldlStep ::
+               Map Word32 [Word32] -> (Word32, Word32) -> Map Word32 [Word32]
+        foldlStep m (acc, addr) = m & at acc . non [] %~ (addr :)
+        addrsMap :: Map Word32 [Word32]
+        addrsMap = foldl' foldlStep mempty _wusAddrs
+        toAccountNode :: (Word32, Text) -> WalletTree
+        toAccountNode (accIdx, accName) =
+            Node
+                { rootLabel =
+                      WalletTreeItem
+                          { wtiLabel = Just accName
+                          , wtiPath = [fromIntegral accIdx]
+                          , wtiShowPath = True
+                          }
+                , subForest =
+                      map (toAddressNode accIdx) $ addrsMap ^. at accIdx .
+                      non []
+                }
+        toAddressNode :: Word32 -> Word32 -> WalletTree
+        toAddressNode accIdx addrIdx =
+            pure $
+            WalletTreeItem
+                { wtiLabel = Nothing
+                , wtiPath = map fromIntegral [accIdx, addrIdx]
+                , wtiShowPath = True
+                }
