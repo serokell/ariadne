16da48e3d4b796c131d3dae5fdbde007393e4a00
goodsoft <goodsoft@goodsoft.lv>
2018-04-03 18:44:51 +0300

    Output help entries like in Auxx

--- ariadne/src/Ariadne/Help.hs
+++ ariadne/src/Ariadne/Help.hs
@@ -1,9 +1,11 @@
-module Ariadne.Help where
+module Ariadne.Help (generateKnitHelp) where
 
 import Universum
 import IiExtras
-import Text.PrettyPrint.ANSI.Leijen as PP
+import Pos.Util.Justify (leftAlign)
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
 import qualified Knit
+import qualified Data.Text as T
 
 generateKnitHelp :: forall components.
   ( AllConstrained (Knit.ComponentCommandProcs components) components
@@ -12,5 +14,53 @@ generateKnitHelp :: forall components.
 generateKnitHelp _execCtxs =
   let
     procs = Knit.commandProcs @components
-    helps = fmap (\(Some cp) -> Knit.cpName cp) procs
-  in fmap (PP.text . show) helps
+  in fmap (PP.text . T.unpack) $ T.lines $ mkHelpMessage procs
+
+commandHelp :: Knit.CommandProc components component -> Text
+commandHelp Knit.CommandProc{..} =
+    let
+        parameters = Knit.getParameters cpArgumentConsumer
+        name = \case
+          Knit.ProcedureName n -> pretty n
+          Knit.OperatorName o -> op o
+        op = \case
+          Knit.OpUnit -> "( )"
+          Knit.OpSemicolon -> "(;)"
+        prefixes = name cpName : repeat (T.replicate (T.length $ name cpName) " ")
+        helpLines = map (\l -> "-- " <> l <> "\n") $ leftAlign 40 cpHelp
+        parameterLines =
+            if null parameters
+            then [""]
+            else map parameterHelp parameters
+        commandDesc = T.intercalate "\n" $
+            zipWith (\p h -> p <> " " <> h) prefixes parameterLines
+    in
+        T.concat helpLines <> commandDesc
+
+parameterHelp :: (Knit.Name, Knit.TypeName, Knit.SomeArgCardinality) -> Text
+parameterHelp (name, tn, ac) = pretty name <> ": " <> withArgCardinality ac (withTypeName tn NeedWrap)
+
+data NeedWrap = NeedWrap | DontNeedWrap
+
+withArgCardinality :: Knit.SomeArgCardinality -> Text -> Text
+withArgCardinality (Knit.SomeArgCardinality ac) = case ac of
+    Knit.ArgCardSingle -> identity
+    Knit.ArgCardOpt    -> (<> "?")
+    Knit.ArgCardMany   -> (<> "*")
+    Knit.ArgCardSome   -> (<> "+")
+
+withTypeName :: Knit.TypeName -> NeedWrap -> Text
+withTypeName (Knit.TypeName t) _ = t
+withTypeName (Knit.TypeNameEither tn1 tn2) needWrap =
+    case needWrap of
+        DontNeedWrap -> t'
+        NeedWrap     -> wrap t'
+  where
+    wrap t = "(" <> t <> ")"
+    t' = withTypeName tn1 DontNeedWrap <> " | " <>
+         withTypeName tn2 DontNeedWrap
+
+mkHelpMessage :: [Some (Elem components) (Knit.CommandProc components)] -> Text
+mkHelpMessage cps =
+    "Available commands:\n \n" <>
+    mconcat (map (\(Some cp) -> commandHelp cp <> "\n \n") cps)
