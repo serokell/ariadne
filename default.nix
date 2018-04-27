20ada669bd3e106e4dfefe9b5b3a4e698beada46
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-04-27 04:16:54 +0200

    CI: remove tmp directory so it does not end up in rpath

--- default.nix
+++ default.nix
@@ -13,7 +13,11 @@ buildStack {
   src = lib.cleanSource ./.;
 
   overrides = final: previous: with nixpkgs.haskell.lib; {
-    ariadne = addBuildTool previous.ariadne git;
+    ariadne = overrideCabal previous.ariadne (drv: {
+      buildTools = (drv.buildTools or []) ++ [ git ];
+      # https://github.com/NixOS/nixpkgs/issues/25585
+      preFixup = ''rm -rf "$(pwd)" '';
+    });
     ariadne-qt-ui = disableLibraryProfiling previous.ariadne-qt-ui;
     qtah-cpp = overrideCabal previous.qtah-cpp (self: {
       librarySystemDepends = (self.librarySystemDepends or []) ++ [ nixpkgs.qt5.qtbase ];
