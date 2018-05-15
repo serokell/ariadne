fea78ff3806a227baada97419c80e77e46450fa3
Ivan Gromakovskii <gie1994@gmail.com>
2018-05-15 19:50:58 +0300

    [AD-106] Add Wallet kernel
    Copied iohk's wallet kernel

---
+++ ariadne/src/Ariadne/Wallet/Cardano/Kernel/DB/BlockMeta.hs
@@ -0,0 +1,42 @@
+-- | Block metadata conform the wallet specification
+module Ariadne.Wallet.Cardano.Kernel.DB.BlockMeta (
+    -- * Block metadata
+    BlockMeta(..)
+    -- ** Lenses
+  , blockMetaSlotId
+  ) where
+
+import Universum
+
+import Control.Lens.TH (makeLenses)
+import qualified Data.Map.Strict as Map
+import Data.SafeCopy (base, deriveSafeCopy)
+
+import qualified Pos.Core as Core
+
+import Ariadne.Wallet.Cardano.Kernel.DB.InDb
+
+{-------------------------------------------------------------------------------
+  Block metadata
+-------------------------------------------------------------------------------}
+
+-- | Block metadata
+data BlockMeta = BlockMeta {
+      -- | Slot each transaction got confirmed in
+      _blockMetaSlotId :: InDb (Map Core.TxId Core.SlotId)
+    }
+
+makeLenses ''BlockMeta
+deriveSafeCopy 1 'base ''BlockMeta
+
+-- | Monoid instance to update 'BlockMeta' in 'applyBlock' (see wallet spec)
+instance Monoid BlockMeta where
+  mempty = BlockMeta {
+        _blockMetaSlotId = InDb Map.empty
+      }
+  a `mappend` b = BlockMeta {
+        _blockMetaSlotId = combineUsing (liftA2 Map.union) _blockMetaSlotId
+      }
+    where
+      combineUsing :: (a -> a -> a) -> (BlockMeta -> a) -> a
+      combineUsing op f = f a `op` f b
