24c426f31fa0852df73c49c7c54fde1bf113cbe8
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-05-23 14:44:54 +0200

    Update CI

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 let
-  overlay = import ''${builtins.fetchGit "ssh://git@github.com:/serokell/serokell-overlay.git"}/pkgs'';
+overlay = import ''${builtins.fetchGit {
+  url = "ssh://git@github.com:/serokell/serokell-overlay.git";
+  ref = "master";}}/pkgs'';
   nixpkgs = import (builtins.fetchTarball "https://github.com/serokell/nixpkgs/archive/master.tar.gz") {
     overlays = [ overlay ];
   };
@@ -11,18 +13,18 @@ buildStack {
   package = "ariadne";
   src = lib.cleanSource ./.;
 
-  overrides = final: previous: with haskell.lib; {
+  overrides = final: previous: with nixpkgs.haskell.lib; {
     ariadne = overrideCabal previous.ariadne (drv: {
       buildTools = (drv.buildTools or []) ++ [ git ];
       # https://github.com/NixOS/nixpkgs/issues/25585
-      preFixup = ''rm -rf "$(pwd)"'';
+      preFixup = ''rm -rf "$(pwd)" '';
     });
     ariadne-qt-ui = disableLibraryProfiling previous.ariadne-qt-ui;
     qtah-cpp = overrideCabal previous.qtah-cpp (self: {
-      librarySystemDepends = (self.librarySystemDepends or []) ++ [ qt5.qtbase ];
+      librarySystemDepends = (self.librarySystemDepends or []) ++ [ nixpkgs.qt5.qtbase ];
     });
     qtah = overrideCabal previous.qtah (self: {
-      libraryToolDepends = (self.libraryToolDepends or []) ++ [ qt5.qtbase ];
+      libraryToolDepends = (self.libraryToolDepends or []) ++ [ nixpkgs.qt5.qtbase ];
     });
   };
 }
