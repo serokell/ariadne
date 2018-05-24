5086d8e36ef30c3ef2d1d95d6c44c42c08a1e2a6
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-05-24 16:59:15 +0200

    release: check that all required paths are reachable in the mount

--- release.nix
+++ release.nix
@@ -5,11 +5,19 @@ overlay = import ''${builtins.fetchGit {
   nixpkgs = import (builtins.fetchTarball "https://github.com/serokell/nixpkgs/archive/master.tar.gz") {
     overlays = [ overlay ];
 };
+envPaths = [
+  "/etc/resolv.conf"
+  "/etc/nsswitch.conf"
+  "/etc/protocols"
+  "/etc/services"
+  "$TERMINFO"
+  "$LOCALE_ARCHIVE"
+];
 ariadne = import ./.;
 nix-bundle = import (builtins.fetchGit "https://github.com/serokell/nix-bundle") { inherit nixpkgs; };
 in
   with nixpkgs;
-rec {5
+rec {
   ariadne-bin = runCommand "ariadne-bin-${ariadne.version}" {
     inherit (ariadne) version;
   } ''
@@ -19,6 +27,14 @@ rec {5
     target = ariadne-bin;
     run = "/bin/ariadne";
     preStart = ''
+      ${nixpkgs.lib.concatMapStringsSep "\n" (path: ''
+        if [[ $(realpath ${path}) == /nix/store/* ]]; then
+          echo "ironically, you can't use this nix-bundle on nixos"
+          echo "${path} symlinks to an entry in the nix store, which we hide."
+          echo "try the nix-specific installation instructions instead! :)"
+          exit 1
+        fi
+    '') envPaths}
       echo "using working directory ~/ariadne"
       mkdir -p $HOME/ariadne
       export LOCALE_ARCHIVE="${nixpkgs.glibcLocales.override { allLocales = false; }}/lib/locale/locale-archive";
@@ -26,11 +42,3 @@ rec {5
     workingDir = "$HOME/ariadne";
   };
 }
-# extraPaths = [
-#   "/etc/resolv.conf"
-#   "/etc/nsswitch.conf"
-#   "/etc/protocols"
-#   "/etc/services"
-#   $TERMINFO
-#   $LOCALE_ARCHIVE
-# ]
