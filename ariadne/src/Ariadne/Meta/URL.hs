14ec39407bed2a2ed46ca903ecbfb02eaaf68bc7
Ivan Gromakovskii <gie1994@gmail.com>
2018-05-29 21:03:21 +0300

    [AD-139] Put URL of the website into the help message

---
+++ ariadne/src/Ariadne/Meta/URL.hs
@@ -0,0 +1,11 @@
+-- | URL of the Ariadne website.
+
+module Ariadne.Meta.URL
+       ( ariadneURL
+       ) where
+
+import Universum
+
+-- | URL of the Ariadne website.
+ariadneURL :: Text
+ariadneURL = "https://serokell.io/ariadne/"
