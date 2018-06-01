d21380c6969900656042f2804b08e6c739a8099a
Dolgakov <dolgakovv@gmail.com>
2018-06-02 02:36:26 +0300

    [AD-128] Remove CId Wal

--- ariadne/src/Ariadne/Wallet/Cardano/Kernel/Decrypt.hs
+++ ariadne/src/Ariadne/Wallet/Cardano/Kernel/Decrypt.hs
@@ -7,59 +7,32 @@ module Ariadne.Wallet.Cardano.Kernel.Decrypt
        , isTxEntryInteresting
        , buildTHEntryExtra
 
-       , WalletDecrCredentials
-       , eskToWalletDecrCredentials
+       , HDPassphrase
+       , eskToHDPassphrase
        , selectOwnAddresses
        , decryptAddress
        ) where
 
 import Universum
 
-import Data.Hashable (Hashable(..))
 import Data.List ((!!))
 import qualified Data.List.NonEmpty as NE
 import Pos.Client.Txp.History (TxHistoryEntry(..))
 import Pos.Core
   (Address(..), ChainDifficulty, Timestamp, aaPkDerivationPath,
-  addrAttributesUnwrapped, makeRootPubKeyAddress)
+  addrAttributesUnwrapped)
 import Pos.Core.Txp
   (Tx(..), TxIn(..), TxOut, TxOutAux(..), TxUndo, toaOut, txOutAddress)
 import Pos.Crypto
   (EncryptedSecretKey, HDPassphrase, WithHash(..), deriveHDPassphrase,
   encToPublic, unpackHDAddressAttr)
-import Pos.Util.Servant (encodeCType)
 import Serokell.Util (enumerate)
 
--- | Client hash
-newtype CHash = CHash Text
-    deriving (Show, Eq, Ord, Generic, Buildable)
-
-instance Hashable CHash where
-    hashWithSalt s (CHash h) = hashWithSalt s h
-
-newtype CId w = CId CHash
-    deriving (Show, Eq, Ord, Generic, Hashable, Buildable)
-
--- instance Buildable (SecureLog $ CId w) where
---     build _ = "<id>"
-
--- | Marks address as belonging to wallet set.
-data Wal = Wal
-    deriving (Show, Generic)
-
 data WAddressMeta = WAddressMeta
-    { _wamWalletId     :: CId Wal
-    , _wamAccountIndex :: Word32
+    { _wamAccountIndex :: Word32
     , _wamAddressIndex :: Word32
     , _wamAddress      :: Address
     } deriving (Eq, Ord, Show, Generic, Typeable)
-
--- makeClassy ''WAddressMeta
--- instance Hashable WAddressMeta
--- instance Buildable WAddressMeta where
---     build WAddressMeta{..} =
---         F.bprint (F.build%"@"%F.build%"@"%F.build%" ("%F.build%")")
---         _wamWalletId _wamAccountIndex _wamAddressIndex _wamAddress
 
 type OwnTxInOuts = [((TxIn, TxOutAux), WAddressMeta)]
 
@@ -82,11 +55,11 @@ isTxEntryInteresting THEntryExtra{..} =
     else Nothing
 
 buildTHEntryExtra
-    :: WalletDecrCredentials
+    :: HDPassphrase
     -> (WithHash Tx, TxUndo)
     -> (Maybe ChainDifficulty, Maybe Timestamp)
     -> THEntryExtra
-buildTHEntryExtra wdc (WithHash tx txId, NE.toList -> undoL) (mDiff, mTs) =
+buildTHEntryExtra hdPass (WithHash tx txId, NE.toList -> undoL) (mDiff, mTs) =
     let (UnsafeTx (NE.toList -> inps) (NE.toList -> outs) _) = tx
         toTxInOut (idx, out) = (TxInUtxo txId idx, TxOutAux out)
 
@@ -96,33 +69,30 @@ buildTHEntryExtra wdc (WithHash tx txId, NE.toList -> undoL) (mDiff, mTs) =
         txInputs = map (toaOut . snd) resolvedInputs
 
         theeInputs :: [((TxIn, TxOutAux), WAddressMeta)]
-        theeInputs = selectOwnAddresses wdc (txOutAddress . toaOut . snd) resolvedInputs
+        theeInputs = selectOwnAddresses hdPass (txOutAddress . toaOut . snd) resolvedInputs
         theeOutputsRaw :: [((Word32, TxOut), WAddressMeta)]
-        theeOutputsRaw = selectOwnAddresses wdc (txOutAddress . snd) (enumerate outs)
+        theeOutputsRaw = selectOwnAddresses hdPass (txOutAddress . snd) (enumerate outs)
         theeOutputs = map (first toTxInOut) theeOutputsRaw
         theeTxEntry = THEntry txId tx mDiff txInputs txOutgoings mTs in
     THEntryExtra {..}
 
-type WalletDecrCredentials = (HDPassphrase, CId Wal)
-
-eskToWalletDecrCredentials :: EncryptedSecretKey -> WalletDecrCredentials
-eskToWalletDecrCredentials encSK = do
+eskToHDPassphrase :: EncryptedSecretKey -> HDPassphrase
+eskToHDPassphrase encSK = do
     let pubKey = encToPublic encSK
     let hdPass = deriveHDPassphrase pubKey
-    let wCId = encodeCType $ makeRootPubKeyAddress pubKey
-    (hdPass, wCId)
+    hdPass
 
 selectOwnAddresses
-    :: WalletDecrCredentials
+    :: HDPassphrase
     -> (a -> Address)
     -> [a]
     -> [(a, WAddressMeta)]
-selectOwnAddresses wdc getAddr =
-    mapMaybe (\a -> (a,) <$> decryptAddress wdc (getAddr a))
+selectOwnAddresses hdPass getAddr =
+    mapMaybe (\a -> (a,) <$> decryptAddress hdPass (getAddr a))
 
-decryptAddress :: WalletDecrCredentials -> Address -> Maybe WAddressMeta
-decryptAddress (hdPass, wCId) addr = do
+decryptAddress :: HDPassphrase -> Address -> Maybe WAddressMeta
+decryptAddress hdPass addr = do
     hdPayload <- aaPkDerivationPath $ addrAttributesUnwrapped addr
     derPath <- unpackHDAddressAttr hdPass hdPayload
     guard $ length derPath == 2
-    pure $ WAddressMeta wCId (derPath !! 0) (derPath !! 1) addr
+    pure $ WAddressMeta (derPath !! 0) (derPath !! 1) addr
