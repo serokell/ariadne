bdf17bca17e863355bdd8a4278df87e15141ae85
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-18 16:56:07 +0300

    ci: try to use stackClosure

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { withQt ? true }:
 
 let
-  overlay = import ''${builtins.fetchTarball "https://github.com/serokell/serokell-public-overlay/archive/public.tar.gz"}/pkgs'';
+  overlay = import ''${builtins.fetchGit "ssh://git@github.com/serokell/serokell-overlay"}/pkgs'';
   nixpkgs = import (builtins.fetchTarball "https://github.com/serokell/nixpkgs/archive/master.tar.gz") {
     overlays = [ overlay ];
   };
@@ -9,39 +9,39 @@ in
 
 with nixpkgs;
 
-buildStackApplication {
-  package = "ariadne";
-  src = lib.cleanSource ./.;
-  ghc = haskell.compiler.ghc822;
-
-  overrides = final: previous: with haskell.lib; {
-    ariadne = haskell.lib.doCheck (overrideCabal previous.ariadne (drv: {
-      buildTools = (drv.buildTools or []) ++ [ git ];
-      # https://github.com/NixOS/nixpkgs/issues/25585
-      preFixup = ''rm -rf "$(pwd)"'';
+let
+  closure = (stackClosure haskell.compiler.ghc802 ./.).override {
+    overrides = final: previous: with haskell.lib; {
+      ariadne = overrideCabal previous.ariadne (drv: {
+        buildTools = (drv.buildTools or []) ++ [ git ];
+        # https://github.com/NixOS/nixpkgs/issues/25585
+        preFixup = ''rm -rf "$(pwd)"'';
       
-      enableSharedExecutables = withQt;
-      buildTarget = if (!withQt) then "lib:ariadne exe:ariadne" else "";
+        enableSharedExecutables = withQt;
+        buildTarget = if (!withQt) then "lib:ariadne exe:ariadne" else "";
       
-      # ugly hack: installer can't deal with missing ariadne-qt
-      preInstall = lib.optionalString (!withQt) ''
-        mkdir -p dist/build/ariadne-qt
-        cp dist/build/ariadne/ariadne dist/build/ariadne-qt/ariadne-qt
-      '';
+        # ugly hack: installer can't deal with missing ariadne-qt
+        preInstall = lib.optionalString (!withQt) ''
+          mkdir -p dist/build/ariadne-qt
+          cp dist/build/ariadne/ariadne dist/build/ariadne-qt/ariadne-qt
+        '';
       
-      postInstall = lib.optionalString (!withQt) ''
-        rm $out/bin/ariadne-qt
-      '';
-    }));
+        postInstall = lib.optionalString (!withQt) ''
+          rm $out/bin/ariadne-qt
+        '';
+      });
 
-    ariadne-qt-ui = disableLibraryProfiling previous.ariadne-qt-ui;
-
-    qtah-cpp = overrideCabal previous.qtah-cpp (self: {
-      librarySystemDepends = (self.librarySystemDepends or []) ++ [ qt5.qtbase ];
-    });
+      ariadne-qt-ui = disableLibraryProfiling previous.ariadne-qt-ui;
     
-    qtah = overrideCabal previous.qtah (self: {
-      libraryToolDepends = (self.libraryToolDepends or []) ++ [ qt5.qtbase ];
-    });
+      qtah-cpp = overrideCabal previous.qtah-cpp (self: {
+        librarySystemDepends = (self.librarySystemDepends or []) ++ [ qt5.qtbase ];
+      });
+    
+      qtah = overrideCabal previous.qtah (self: {
+        libraryToolDepends = (self.libraryToolDepends or []) ++ [ qt5.qtbase ];
+      });
+    };
   };
-}
+in
+
+{ inherit (closure) ariadne-vty ariadne-qt; }
