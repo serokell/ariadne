c9f9c497e5474a4ac38b8b7b97d282a302824a7a
Ivan Gromakovskii <gie1994@gmail.com>
2018-06-22 14:35:39 +0300

    Enable ariadne tests

--- default.nix
+++ default.nix
@@ -10,20 +10,20 @@ with nixpkgs;
 let
   closure = (stackClosure haskell.compiler.ghc822 ./.).override {
     overrides = final: previous: with haskell.lib; {
-      ariadne = overrideCabal previous.ariadne (super: {
+      ariadne = haskell.lib.doCheck (overrideCabal previous.ariadne (super: {
         buildTools = [ git ];
-      });
+      }));
 
       ariadne-qt = disableLibraryProfiling (overrideCabal previous.ariadne-qt (super: {
         # https://github.com/NixOS/nixpkgs/issues/25585
         # RPATH of binary contains a forbidden reference to /tmp/nix-build...
         preFixup = ''rm -rf "$(pwd)"'';
       }));
-    
+
       qtah-cpp = overrideCabal previous.qtah-cpp (super: {
         librarySystemDepends = [ qt5.qtbase ];
       });
-    
+
       qtah = overrideCabal previous.qtah (super: {
         libraryToolDepends = [ qt5.qtbase ];
       });
