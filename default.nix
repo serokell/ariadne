7e801e180dee76dbcfe0005d401ab54aabd5b216
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-26 19:59:49 +0300

    Fix Knit stack2nix closure

--- default.nix
+++ default.nix
@@ -20,7 +20,9 @@ let
         preFixup = ''rm -rf "$(pwd)"'';
       }));
 
-      knit = haskell.lib.doCheck previous.knit;
+      knit = haskell.lib.doCheck (overrideCabal previous.knit (super: with final; {
+        buildDepends = (super.buildDepends or []) ++ [ hspec universum ];
+      }));
 
       qtah-cpp = overrideCabal previous.qtah-cpp (super: {
         librarySystemDepends = [ qt5.qtbase ];
