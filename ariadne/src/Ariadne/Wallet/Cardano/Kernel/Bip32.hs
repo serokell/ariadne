9a337adb83c829a41476c43f2602bb982c05a6f6
Ivan Gromakovskii <gie1994@gmail.com>
2018-06-28 15:32:14 +0300

    [AD-135] Make BIP-32 functions more granular and reusable (#211)
    [AD-135] Make BIP-32 functions more granular and reusable
    
    1. Part of BIP-32 logic was moved from KeyStorage.hs to a new module,
       so that it logic can be easier used from other places.
    2. Previously there was a function to derive a key by path and construct
       an address (all at once). I split it into two functions basically.

---
+++ ariadne/src/Ariadne/Wallet/Cardano/Kernel/Bip32.hs
@@ -0,0 +1,43 @@
+-- | Helpers to work with BIP-32 implementation in Cardano.
+
+module Ariadne.Wallet.Cardano.Kernel.Bip32
+       ( deriveHDSecretKeyByPath
+       , makePubKeyHdwAddressUsingPath
+       ) where
+
+import Universum
+
+import Named (Named(..))
+
+import Pos.Core.Common (Address, IsBootstrapEraAddr(..), makePubKeyHdwAddress)
+import Pos.Crypto.HD
+  (ShouldCheckPassphrase(..), deriveHDPassphrase, deriveHDSecretKey,
+  packHDAddressAttr)
+import Pos.Crypto.Signing (EncryptedSecretKey, PassPhrase, PublicKey)
+
+-- | Like 'deriveHDSecretKey' from Cardano, but can derive not only
+-- direct descendant.
+deriveHDSecretKeyByPath
+    :: ShouldCheckPassphrase
+    -> PassPhrase
+    -> EncryptedSecretKey
+    -> [Word32]
+    -> Maybe EncryptedSecretKey
+deriveHDSecretKeyByPath shouldCheck pp sk derPath =
+    foldM
+        (\sk' (check, idx) -> deriveHDSecretKey check pp sk' idx)
+        sk
+        -- check passphrase at most once
+        (zip (shouldCheck : repeat (ShouldCheckPassphrase False)) derPath)
+
+-- | Like 'makePubKeyHdwAddress', but also creates HDPassphrase
+-- internally using derivation path and root public key.
+makePubKeyHdwAddressUsingPath ::
+       IsBootstrapEraAddr
+    -> [Word32]
+    -> PublicKey `Named` "root" -> PublicKey `Named` "address" -> Address
+makePubKeyHdwAddressUsingPath era derPath (Named rootPK) (Named addrPK) =
+    makePubKeyHdwAddress era hdPayload addrPK
+  where
+    hdPP = deriveHDPassphrase rootPK
+    hdPayload = packHDAddressAttr hdPP derPath
