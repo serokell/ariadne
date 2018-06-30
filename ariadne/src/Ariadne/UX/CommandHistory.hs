41b009daf4c82ab21a3d663d58abee312627493d
Mitchell Vitez <mitchell@vitez.me>
2018-06-30 13:54:15 -0700

    Fix command history paging, add new behavior for edges of history

--- ariadne/src/Ariadne/UX/CommandHistory.hs
+++ ariadne/src/Ariadne/UX/CommandHistory.hs
@@ -16,46 +16,68 @@ data Row = Row Int T.Text deriving (Show)
 instance FromRow Row where
   fromRow = Row <$> field <*> field
 
+data CounterChange = Previous | Next
+  deriving Eq
+
 data CommandHistory =
     CommandHistory
     { historyFile :: FilePath
     , counter :: IORef Int
     , currentPrefix :: IORef Text
+    , lastCommand :: IORef Text
     }
 
 openCommandHistory :: FilePath -> IO CommandHistory
 openCommandHistory historyFile = do
     currentPrefix <- newIORef ""
+    lastCommand <- newIORef ""
     currentCounter <- newIORef 0
     withConnection historyFile $
       \conn -> execute_ conn "CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY, command TEXT)"
-    let ch = CommandHistory historyFile currentCounter currentPrefix
+    let ch = CommandHistory historyFile currentCounter currentPrefix lastCommand
     resetCounter ch
     return ch
 
 toPrevCommand :: CommandHistory -> IO (Maybe Text)
 toPrevCommand ch =
-    changeCommand ch 1
+    changeCommand ch Previous
 
 toNextCommand :: CommandHistory -> IO (Maybe Text)
 toNextCommand ch =
-    changeCommand ch (-1)
+    changeCommand ch Next
 
-changeCommand :: CommandHistory -> Int -> IO (Maybe Text)
+direction :: CounterChange -> Int
+direction Previous = (-1)
+direction Next = 1
+
+changeCommand :: CommandHistory -> CounterChange -> IO (Maybe Text)
 changeCommand ch counterChange = do
-    newCounter <- (+counterChange) <$> readIORef (counter ch)
-    resetCounter ch
     prefix <- readIORef (currentPrefix ch)
-    let comp = if counterChange < 0 then "<=" else ">=" :: Text
-    let order = if counterChange < 0 then "ASC" else "DESC" :: Text
+    let prefix' = prefix <> "%"
 
-    withConnection (historyFile ch) $ \conn -> do
-      queryResult <- query conn "SELECT * FROM history WHERE command LIKE '?%' AND id ? ? ORDER BY id ? LIMIT 1" (prefix, comp, newCounter, order)
-      case queryResult of
-        [Row _ cmd] -> return $ Just cmd
-        _ -> do
-          resetCounter ch
-          return Nothing
+    count <- readIORef (counter ch)
+    writeIORef (counter ch) (count + direction counterChange)
+
+    queryResult <- withConnection (historyFile ch) $ \conn ->
+      case counterChange of
+        Previous ->
+          query conn "SELECT * FROM history WHERE command LIKE ? AND id < ? ORDER BY id DESC LIMIT 1" (prefix', count)
+        Next ->
+          query conn "SELECT * FROM history WHERE command LIKE ? AND id > ? ORDER BY id ASC LIMIT 1" (prefix', count)
+
+    case queryResult of
+      [Row rowId cmd] -> do
+        writeIORef (counter ch) rowId
+        writeIORef (lastCommand ch) cmd
+        return $ Just cmd
+      _ ->
+        case counterChange of
+          Previous -> do
+            last <- readIORef (lastCommand ch)
+            return $ Just last
+          Next -> do
+            resetCounter ch
+            return $ Just prefix
 
 setPrefix :: CommandHistory -> Text -> IO ()
 setPrefix ch (T.strip -> prefix) = do
@@ -64,17 +86,16 @@ setPrefix ch (T.strip -> prefix) = do
 
 addCommand :: CommandHistory -> Text -> IO ()
 addCommand ch (T.strip -> command) = do
-    resetCounter ch
     writeIORef (currentPrefix ch) ""
-
     unless (null command) $
       withConnection (historyFile ch) $
         \conn -> execute conn "INSERT INTO history (command) VALUES (?)" (Only command)
+    resetCounter ch
 
 resetCounter :: CommandHistory -> IO ()
 resetCounter ch =
   withConnection (historyFile ch) $ \conn -> do
-    queryResult <- query_ conn "SELECT MAX(id) FROM history"
+    queryResult <- query_ conn "SELECT COALESCE(MAX(id), 0) FROM history" :: IO [[Int]]
     case queryResult of
-      [[maxId]] -> writeIORef (counter ch) maxId
+      [[maxId]] -> writeIORef (counter ch) (maxId + 1)
       _ -> return ()
