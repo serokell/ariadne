ea8c25eedae15994127e3be2d6b7e75f11f1bc1e
Lionel Miller <delonorm@gmail.com>
2018-07-18 19:05:57 +0300

    [AD-167] Restore call to verifyBlocks

--- ariadne/test/backend/UTxO/Verify.hs
+++ ariadne/test/backend/UTxO/Verify.hs
@@ -308,8 +308,9 @@ slogVerifyBlocks curSlot leaders lastSlots blocks = do
             when (block ^. genBlockLeaders /= leaders) $
             throwError "Genesis block leaders don't match with LRC-computed"
         _ -> pass
-    --verResToMonadError formatAllErrors $
-    --    verifyBlocks curSlot dataMustBeKnown adoptedBVD leaders blocks
+    let blocksList = OldestFirst (toList (getOldestFirst blocks))
+    verResToMonadError formatAllErrors $
+       verifyBlocks curSlot dataMustBeKnown adoptedBVD leaders blocksList
 
     -- Here we need to compute 'SlogUndo'. When we add apply a block,
     -- we can remove one of the last slots stored in
