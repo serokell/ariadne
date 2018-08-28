63dd1bd02888f3c85670d278d58edd14031e03c0
Lionel Miller <delonorm@gmail.com>
2018-08-28 18:10:36 +0300

    [AD-326] Prettify import_wallet.sh

--- ariadne/cardano/import_wallet.sh
+++ ariadne/cardano/import_wallet.sh
@@ -8,14 +8,24 @@ ARIADNE_WALLET_PATH=ariadne/cardano/src/Ariadne/Wallet/Cardano
 mkdir -p ${ARIADNE_WALLET_PATH}
 
 echo "Copying code from cardano-sl"
-rsync -av --delete "${CARDANO_SL_PATH}"/wallet-new/src/Cardano/Wallet/Kernel* ${ARIADNE_WALLET_PATH}
-rsync -av --delete "${CARDANO_SL_PATH}"/wallet-new/src/Cardano/Wallet/{WalletLayer.hs,WalletLayer/Kernel.hs,WalletLaye/Types.hs} ${ARIADNE_WALLET_PATH}/WalletLayer
+rsync -av --delete \
+    "${CARDANO_SL_PATH}"/wallet-new/src/Cardano/Wallet/Kernel* \
+    ${ARIADNE_WALLET_PATH}
+rsync -av --delete \
+    "${CARDANO_SL_PATH}"/wallet-new/src/Cardano/Wallet/{WalletLayer.hs,WalletLayer/Kernel.hs,WalletLayer/Types.hs} \
+    ${ARIADNE_WALLET_PATH}/WalletLayer
 
 echo "Renaming modules..."
-find ${ARIADNE_WALLET_PATH} -name '*.hs' -exec sed -e 's/\bCardano\.Wallet\./Ariadne.Wallet.Cardano./g' -i {} \;
+find ${ARIADNE_WALLET_PATH} \
+    -name '*.hs' \
+    -exec sed -e 's/\bCardano\.Wallet\./Ariadne.Wallet.Cardano./g' -i {} \;
 
 echo "Prettifying with stylish-haskell..."
-find ${ARIADNE_WALLET_PATH}/{Kernel,WalletLayer} -name '*.hs' -exec stylish-haskell -i -v {} \+
+find ${ARIADNE_WALLET_PATH}/{Kernel,WalletLayer} \
+    -name '*.hs' \
+    -exec stylish-haskell -i -v {} \+
 
 echo "Here are all new modules:"
-find ${ARIADNE_WALLET_PATH} -name '*.hs' -exec sed -n -e 's/^module \([^ (]\+\).*/\1/p' {} \; | sort
+find ${ARIADNE_WALLET_PATH} \
+    -name '*.hs' \
+    -exec sed -n -e 's/^module \([^ (]\+\).*/\1/p' {} \; | sort
