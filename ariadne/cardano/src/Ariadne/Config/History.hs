a45e1960800f6d6417136cde535f74cec03b0462
pasqu4le <pasqu4le@gmail.com>
2018-09-19 12:55:52 +0200

    [AD-175] use the correct configuration for Updates and History in CLI (with tests)

--- ariadne/cardano/src/Ariadne/Config/History.hs
+++ ariadne/cardano/src/Ariadne/Config/History.hs
@@ -13,7 +13,7 @@ import Dhall.Parser (Src(..))
 import Dhall.TypeCheck (X)
 import System.FilePath ((</>))
 
-import Ariadne.Config.DhallUtil (parseField)
+import Ariadne.Config.DhallUtil (injectFilePath, interpretFilePath, parseField)
 import Ariadne.Util (postfixLFields)
 
 defaultHistoryConfig :: FilePath -> HistoryConfig
@@ -40,11 +40,28 @@ instance D.Interpret HistoryConfig where
   autoWith _ = D.Type extractOut expectedOut
     where
       extractOut (RecordLit fields) = do
-        hcPath <- parseFieldHistory fields "hcPath" D.string
+        hcPath <- parseFieldHistory fields "hcPath" interpretFilePath
         return HistoryConfig {..}
       extractOut _ = Nothing
 
       expectedOut =
         Record
             (Map.fromList
-                [(historyFieldModifier "hcPath", D.expected D.string)])
+                [(historyFieldModifier "hcPath", D.expected interpretFilePath)])
+
+instance D.Inject HistoryConfig where
+    injectWith _ = injectHistoryConfig
+
+injectHistoryConfig :: D.InputType HistoryConfig
+injectHistoryConfig = D.InputType {..}
+  where
+      embed HistoryConfig {..} = RecordLit
+          (Map.fromList
+              [ (historyFieldModifier "hcPath",
+                D.embed injectFilePath hcPath)
+              ])
+
+      declared = Record
+          (Map.fromList
+              [ (historyFieldModifier "hcPath", D.declared injectFilePath)
+              ])
---
+++ ariadne/cardano/test/Test/Ariadne/Update/Arbitrary.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Test.Ariadne.Update.Arbitrary
+       (
+       ) where
+
+import Test.QuickCheck.Arbitrary (Arbitrary(..))
+import Test.QuickCheck.Gen (elements)
+
+import Ariadne.Config.Update (UpdateConfig(..))
+
+import Test.Ariadne.Cardano.Arbitrary (genValidText)
+
+instance Arbitrary UpdateConfig where
+    arbitrary = do
+        ucVersionCheckUrl <- genValidText
+        ucUpdateUrl <- genValidText
+        ucCheckDelay <- elements [3600, 7200..21600]
+        return UpdateConfig {..}
