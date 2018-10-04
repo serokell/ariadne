5d31b616a9ad30eac200bf6846f9e08251b200ad
rvem <rvembox@gmail.com>
2018-10-04 16:27:28 +0300

    [AD-427] Change link to changelog

--- ariadne/core/src/Ariadne/UIConfig.hs
+++ ariadne/core/src/Ariadne/UIConfig.hs
@@ -8,7 +8,7 @@ licenseUrl :: Text
 licenseUrl = "https://serokell.io/ariadne/license"
 
 changelogUrl :: Text
-changelogUrl = "https://github.com/serokell/ariadne/blob/master/CHANGELOG.md"
+changelogUrl = "https://serokell.io/ariadne/changelog"
 
 aboutUrl :: Text
 aboutUrl = "https://github.com/serokell/ariadne"
